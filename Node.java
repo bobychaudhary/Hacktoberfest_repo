7af83a6a2bc953aa45057e823651f3c76b256e2d
Sanat Kumar Dubey <68190043+Sanat0412@users.noreply.github.com>
2022-10-09 21:50:10 +0530

    Update Node.java
    its better approach for that

--- Node.java
+++ Node.java
@@ -1,19 +1,26 @@
-/*   Created by IntelliJ IDEA.
- *   Author: Kshitij Varshney (kshitijvarshne1)
- *   Date: 24-Jun-21
- *   Time: 12:07 PM
- *   File: Node.java
+/**
+ * @Jai_Bajrang_Bali
+ * @Har_Har_Mahadev
  */
+//@Author : Sanat04
 
-package March.mar03_21_NK;
 
-public class Node {
-    public int data;
-    public Node next;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
+public class practice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder res = new StringBuilder();
+        int t = sc.nextInt();
+        //int t=1;
+        while (t-- > 0) {
+           String str= sc.next();
+           char[] ch=str.toCharArray();
+           Arrays.sort(ch);
+           res.append(ch).append("\n");
+        }
+        System.out.println(res);
     }
 }
-
