5e50ef751af3003147af2f5944441502164ef0d0
mangalam1234 <115993850+mangalam1234@users.noreply.github.com>
2022-10-17 17:17:36 +0530

    Update Delete.java
    this is the best version i think

--- Delete.java
+++ Delete.java
@@ -1,28 +1,34 @@
-/*
- * @Jai_Bajrang_Bali
- * @Har_Har_Mahadev
- */
-/*   Author: Sanat Kumar Dubey (sanat04)
- *   File: G.java
- */
+import java.util.*;
 
-import java.util.Scanner;
-
-public class G {
-    public static int help(int n) {
-        String str = String.valueOf(n);
-        return str.length();
-    }
-
+public class A_Linear_keyboard {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            int res = help(i);
-            if (res % 2 != 0) count++;
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String l = sc.nextLine();
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                if(map.containsKey(s.charAt(i))){
+                    continue;
+                }
+                else{
+                    map.put(s.charAt(i),i+1);
+                }
+            }
+            if (l.length() == 0 || l.length() == 1) {
+                System.out.println(0);
+                continue;
+            }
+            int sum = 0;
+            int bb = map.get(l.charAt(0));
+            for (int i = 1; i < l.length(); i++) {
+                int res = Math.abs(bb - map.get(l.charAt(i)));
+                sum += res;
+                bb = map.get(l.charAt(i));
+            }
+            System.out.println(sum);
         }
-        System.out.println(count);
     }
 }
-
