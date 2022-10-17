95aeb80213c51fb86b73a81f6221da58df464216
mangalam1234 <115993850+mangalam1234@users.noreply.github.com>
2022-10-17 17:21:27 +0530

    Update RemoveDuplicate.java

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,25 +1,49 @@
-package LinkedList.SinglyLinkedList;
-public class RemoveDuplicate extends SinglyLinkedlist {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-    //remove duplicates from sorted linked list
-    public ListNode removeDuplicate(){
-        ListNode current=head;
-        while(current!=null && current.next!=null){
-            if(current.data==current.next.data){
-                current.next=current.next.next;
+public class D1_All_are_Same {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i]=sc.nextInt();
+            }
+            ArrayList<Integer> answer=new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int k = Math.abs(arr[i] - arr[j]);
+                    if (k != 0) {
+                        answer.add(k);
+                    }
+                }
+            }
+            int ans=0;
+            for (Integer integer : answer) {
+                ans=gcd(ans,integer);
+
+            }
+            if(ans==0){
+                System.out.println(-1);
             }
             else{
-                current=current.next;
+                System.out.println(ans);
             }
-            return current;
-        
+
         }
     }
+    static int gcd(int a, int b) {
+        if (b > a) {
+            return gcd(b, a);
+        }
 
+        if (b == 0) {
+            return a;
+        }
 
-
-    public static void main(String[] args) {
-        
+        return gcd(b, a % b);
     }
-    
 }
